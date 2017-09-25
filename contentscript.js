9db0deedff3b0fe69fa60c6d251683e5a6a290a0
ITCS'sDeveloper <itcss.developer.me@gmail.com>
2017-09-25 21:07:45 +0700

    Resize Keywords Container to height 450px (V.1.2)
    Improve performance and UI Wording (V.1.2)

--- contentscript.js
+++ contentscript.js
@@ -9,11 +9,24 @@ chrome.extension.onRequest.addListener(
             }
             tempTags = tempTags.substring(0, tempTags.length - 2);
 
-            sendResponse({ data: tempTags, count: x.length, method: "getTag" }); //same as innerText
+            sendResponse({ data: tempTags, count: x.length, method: "getTag" });
         }
         else if (request.method == "getDesc") {
             var desc = document.getElementsByClassName("product-desc")[0].innerHTML;
-            sendResponse({ data: desc, method: "getDesc" }); //same as innerText
+            sendResponse({ data: desc, method: "getDesc" });
+        }
+        else if (request.method == "resizeKeywordContainer") {
+
+            var array = document.getElementsByClassName("keywords_container");
+            for (var i = 0; i < array.length; i++) {
+                array[i].style.height = "450px";
+            }
+
+
+            if (array.length > 0)
+                sendResponse({ length: array.length, method: "resizeKeywordContainer" });
+
+            sendResponse({  });
         }
     }
 );
--- script.js
+++ script.js
@@ -3,47 +3,59 @@
 
 // When the popup HTML has loaded
 window.addEventListener('load', function (evt) {
+    var btnGetKeyWord = document.getElementById('btnGetKeyWord');
     var lblFeedback = document.getElementById('lblFeedback');
     var txtResult = document.getElementById('txtResult');
     var btnCopy = document.getElementById('btnCopy');
-    var btnSave = document.getElementById('btnSave');
-    var tableFav = document.getElementById('tableFav');
+    var btnResize = document.getElementById('btnResize');
 
     // Get Keyword
-    document.getElementById('btnGetKeyWord').onclick = function () {
+    btnGetKeyWord.onclick = function () {
         chrome.tabs.getSelected(null, function (tab) {
             chrome.tabs.sendRequest(tab.id, { method: "getTag" }, function (response) {
-
                 if (response == undefined || response.method !== "getTag" || response.data == '') {
                     lblFeedback.innerHTML = 'Not found tag.';
-                    lblFeedback.style.color = "red";
+                    lblFeedback.className = "alert alert-danger";
                     return;
                 }
 
                 txtResult.innerHTML = response.data;
                 lblFeedback.innerHTML = 'Found ' + response.count + ' tags.';
-                lblFeedback.style.color = "green";
+                lblFeedback.className = "alert alert-success";
             });
         });
     };
-
     // Function Copy
     btnCopy.onclick = function () {
         if (txtResult.innerHTML.indexOf('How to Use') !== -1) {
-            lblFeedback.innerHTML = 'Not found tag.';
-            lblFeedback.style.color = "red";
+            lblFeedback.innerHTML = 'Active when keyword is detected.';
+            lblFeedback.className = "alert alert-danger";
             return;
         }
 
         if (lblFeedback.innerHTML.indexOf('Copied') == -1 && lblFeedback.innerHTML.indexOf('Not found tag.') === -1) {
-            lblFeedback.innerHTML += ' Copied!!';
-            lblFeedback.style.color = "green";
+            lblFeedback.innerHTML = lblFeedback.innerHTML + ' Copied!!';
+            lblFeedback.className = "alert alert-success";
         }
 
         txtResult.select();
         document.execCommand('copy');
     };
+    // Resize Keyword Container 
+    btnResize.onclick = function () {
+        chrome.tabs.getSelected(null, function (tab) {
+            chrome.tabs.sendRequest(tab.id, { method: "resizeKeywordContainer" }, function (response) {
+                if (response == undefined || response.method !== "resizeKeywordContainer" || response.data == '') {
+                    lblFeedback.innerHTML = 'Active on www.submit.shutterstock.com';
+                    lblFeedback.className = "alert alert-danger";
+                    return;
+                }
 
+                lblFeedback.innerHTML = 'Resized ' + response.length + '  Containers.';
+                lblFeedback.className = "alert alert-success";
+            });
+        });
+    };
     // a link to Github
     document.getElementById('aLinkGithub').onclick = function () {
         var newURL = "https://github.com/ITCSsDevloper/shutterstock-keywords";
